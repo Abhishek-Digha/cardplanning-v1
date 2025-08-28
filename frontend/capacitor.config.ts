779385f1e1a6dc2795a33bffc8007ac8f88c9072
abhidigha <abhishek.digha1990@gmail.com>
2025-08-28 10:01:51 +0530

    ios changes added

---
+++ frontend/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.ak47.storypointing',
+  appName: 'Story Pointing Agile',
+  webDir: 'build'
+};
+
+export default config;
